4bc14ab3a62bd1a0addf1a6d53a1637ed35ec20b
Antonio Fernandez <af3036@nyu.edu>
2022-10-20 13:38:21 -0500

    Add files via upload

--- fit_psychometric_funcs.py
+++ fit_psychometric_funcs.py
@@ -1,5 +1,7 @@
-def fit_psy_func(filedir,units)
+def fit_psy_func(filedir,units,stats)
 '''
+Fits individual data and plots mean fit with errorbars 
+
 inputs: filedir: dir to .csv including all performance data in specified units with column labels
                  organized as such: 
                  1st col - x-labels (ie, contrast values)
@@ -21,10 +23,18 @@ inputs: filedir: dir to .csv including all performance data in specified units w
            ...
             2         0.01        3           1        1
             7         0.8         3           1        1
+           ...        ...        ...         ...      ...n
          -----------------------------------------------------
  
         units: if "accuracy" then units assumed to be in % correct from X to 100%
                if "dprime" then assumes units are 0 to infinity
+        stats: if "True" displays freq statistics for parameter values (i.e., ANOVA)
+               if "False" does nothing
+
+
+ CREATED BY: Antonio Fernandez [Oct. 20, 2022]
+    contact: antoniofs23@gmail.com
+   moreinfo: antoniofs23.github.io/web
 '''
 
 
@@ -52,8 +62,8 @@ res_log = stats.linregress(xvals,np.logspace(np.log10(xvals[0]),np.log10(xvals[-
 
 # designated a flag for linear or log spacing
 if res_line.rvalue > res_log.rvalue:
-    flag = 'linear'
+    spacing = 'linear'
 else:
-    flag = 'log'
+    spacing = 'log'
 
 
