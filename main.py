bfbce29bc4e434a69658a4d06355e316926b458d
antoniofs23 <af3036@nyu.edu>
2022-10-27 17:20:37 -0500

    simpler functionally

--- main.py
+++ main.py
@@ -1,62 +1,24 @@
-def fit_psy_func(file,units,chance=0,color=False):
+def fit_psy_func(file,chance=0):
     '''
-    Fits individual data and plots mean fit with errorbars (SEM) 
-
-    inputs: file:    .csv file including all performance data in specified units with column labels
-            organized as such: 
-        1st col - x-labels (ie, contrast values)
-        2nd col - y-data   (ie, % correct or dprime)
-        3rd col - alphanumerical condition labels (ie, if attention task then labels: valid/neutral/invalid OR 1,2,3)
-        4th col - alphanumerical factor labels (ie, if conditions all attention then enter 1 OR attention)
-        5th col - numerical subject labels
-        Example with numerical labels:
-        | contrast | dprime | conditions | factors| subjects |
-        2         0.02        1           1        1
-        7         1.1         1           1        1
-        13        1.6         1           1        1
-        24        1.75        1           1        1
-        46        2.2         1           1        1
-        85        2.25        1           1        1
-        ...
-        2         0.01        2           1        1
-        7         0.8         2           1        1
-        ...
-        2         0.01        3           1        1
-        7         0.8         3           1        1
-        ...        ...        ...         ...      ...n
-        -----------------------------------------------------
-
-        units: if "accuracy" then units assumed to be in % correct from X to 100%
-        if "dprime" then assumes units are 0 to infinity
-        
-        if units = 'accuracy'
-        **the code defaults to fitting a Weibull function
-        **for simplicity cost function is defined as sum squared error**
-        if units = 'dprime'
-        **fits a nakarushton function
-
-        chance: what is chance performance in your task? 
-                if units are accuracy:
-                chance is derived from response alternatives i.e., if your
-                task has two-choices (yes/no) then chance is 50% so enter 0.5
-                if four-choices then chance is 25%  enter 0.25
-                if units are dprime enter 0 regardless of task
-
-        color: specifiy a color per condition ranging from 0 to 1. Will be chosen 
-            pseudo-randomly (from 100 possible) if input is [0]
-            i.e. specify three colors one for each condition via:
-            [[0.1,0.1,0.1],[0.2,0.2,0.2],[0.3,0.3,0.3]]
+    [UNDER CONSTRUCTION]
     
+    Fits specified psychometric function to an individual subject's
+    trial-wise responses and plots fit with bootstrapped errorbars
+    
+    inputs: 
+        file:  .csv file containing trial-wise subject responses
+      chance:  what is chance performance in your task? example if a detection task
+               then there are 2 response alternatives (yes/no) so enter 0.5, 
+               if 4 response alternatives enter 0.25
+
     output:
-        outputs an output.npy file in out_dir with three variables:
-        1) subjects parameters
-        2) subject fits
-        3) fits to group mean 
-
-    **no implementation exists for minimization via negative log likelihood as
-    **that would require single trial data
-
-    CREATED BY: Antonio Fernandez [Oct. 20, 2022]
+        output.npy file in the our_dir containing:
+        1) % correct per condition
+        2) best fit functions per condition
+        3) best fit parameters
+          
+    CREATED BY: Antonio Fernandez (af) [Oct. 20, 2022]
+    last edited: Oct. 27, 2020 af
     contact: antoniofs23@gmail.com
     '''
     import numpy as np
@@ -66,7 +28,6 @@ def fit_psy_func(file,units,chance=0,color=False):
     from scipy import stats
     from scipy.optimize import minimize
     import fitting_funcs as ff
-    from tempfile import TemporaryFile
     
     # change plotting style to seaborn
     #sns.set_theme()
